eaa1118c63605c7436137ef2d3a39b894a5ce902
truonggiang1139 <truonggiang1139@gmail.com>
2024-03-09 01:30:51 +0700

    feat: add loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import { Input } from "./common/components/input";
 import { WeatherGroup } from "./common/components/weather-group";
 import { EmptyState } from "./common/components/empty-state";
 import { GeoCoordinate } from "./types/geo-coordinate";
+import { LoadingState } from "./common/components/loading-state";
 
 function App() {
   const [coordData, setCoorData] = useState<GeoCoordinate>();
+  const [isLoading, setIsloading] = useState(false);
   const [isError, setIsError] = useState<boolean>(false);
 
   const handleSearchCoord = async (cityName: string) => {
     try {
+      setIsloading(true);
       const geoData = await getGeoCoord(cityName);
+      setIsloading(false);
       if (!geoData) {
         throw new Error("Geolocation data not found");
       }
@@ -24,12 +28,11 @@ function App() {
     }
   };
 
-  console.log(coordData);
   return (
     <div className="container">
       <Input onSearchWeather={handleSearchCoord} />
-
-      {coordData ? <WeatherGroup coordData={coordData} /> : <EmptyState isError={isError} />}
+      {isLoading && <LoadingState />}
+      {!isLoading && (coordData ? <WeatherGroup coordData={coordData} /> : <EmptyState isError={isError} />)}
     </div>
   );
 }
