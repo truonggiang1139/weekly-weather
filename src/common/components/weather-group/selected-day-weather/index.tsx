8b1767cff71a8f59216847aae483fee9073372ed
truonggiang1139 <truonggiang1139@gmail.com>
2024-03-09 13:26:31 +0700

    fix: styling some css

--- src/common/components/weather-group/selected-day-weather/index.tsx
+++ src/common/components/weather-group/selected-day-weather/index.tsx
@@ -24,38 +24,44 @@ export function SelectedDayWeather(props: SelectedDayWeatherProps) {
       </div>
 
       <div className="flex">
-        <div className="flex-1 flex items-center relative">
+        <div className="flex-1 flex flex-wrap justify-center items-center">
           <img src={getIconWeather(selectedDay.weather[0].icon)} alt="" />
-          <div className="font-bold text-4xl">
-            {isToday ? Math.floor(Number(selectedDay?.currentTemp)) : Math.floor(selectedDay.temp.max)}°
-          </div>
-          <div className="absolute text-sm top-[30%] right-[40%] text-gray-600">
-            <span
-              onClick={() => onChangeUnit("imperial")}
-              className={classNames("hover:cursor-pointer", {
-                "underline text-black font-bold": unit === "imperial"
-              })}
-              aria-hidden="true"
-            >
-              F
-            </span>
-            /
-            <span
-              onClick={() => onChangeUnit("metric")}
-              className={classNames("hover:cursor-pointer", {
-                "underline text-black font-bold": unit === "metric"
-              })}
-              aria-hidden="true"
-            >
-              C
-            </span>
+
+          <div className="flex">
+            <div className="font-bold text-4xl">
+              {isToday ? Math.floor(Number(selectedDay?.currentTemp)) : Math.floor(selectedDay.temp.max)}°
+            </div>
+
+            <div className=" text-sm  text-gray-600">
+              <span
+                onClick={() => onChangeUnit("imperial")}
+                className={classNames("hover:cursor-pointer", {
+                  "underline text-black font-bold": unit === "imperial"
+                })}
+                aria-hidden="true"
+              >
+                F
+              </span>
+              /
+              <span
+                onClick={() => onChangeUnit("metric")}
+                className={classNames("hover:cursor-pointer", {
+                  "underline text-black font-bold": unit === "metric"
+                })}
+                aria-hidden="true"
+              >
+                C
+              </span>
+            </div>
           </div>
         </div>
         <div className="flex-1 flex items-start flex-col text-left text-sm justify-center">
           <div>Humidity:{selectedDay.humidity}%</div>
+
           <div>
             Wind:{selectedDay.wind_speed} <span>{unit === "metric" ? "metre/sec" : "miles/hour"}</span>
           </div>
+
           {selectedDay.pollution && <div>Air Quality: {AIR_QUALITY_LEVEL[selectedDay.pollution - 1]}</div>}
         </div>
       </div>
